24627b57011a3e24378057093c6e76d043f9e1d4
BLUE <18215676683@163.com>
2019-07-09 19:26:17 +0800

    add argis for js sdk to github

---
+++ 3.9/js/dojo/tdt/BDVecLayer.js
@@ -0,0 +1,79 @@
+define(["dojo/_base/declare",
+    "esri/layers/tiled"],
+    function (declare) {
+        return declare(esri.layers.TiledMapServiceLayer, {
+            constructor: function () {
+
+                this.spatialReference = new esri.SpatialReference({ wkid: 102100 });
+                this.initialExtent = (this.fullExtent = new esri.geometry.Extent(-33526792.7187241, -33526792.7187241, 33526792.7187241, 33526792.7187241, this.spatialReference));
+                this.scale = [
+                    989964351.93082204724409448818898,
+                    494982175.96541102362204724409449,
+                    2578032.1664865157480314960629921,
+                    1289016.0832432578740157480314961,
+                    644508.04162162893700787401574803,
+                    322254.02081081446850393700787402,
+                    161127.01040540723425196850393701,
+                    80563.505202703617125984251968504,
+                    40281.752601351808562992125984252,
+                    20140.876300675904281496062992126,
+                    10070.438150337952140748031496063,
+                    5035.2190751689760703740157480315,
+                    2517.6095375844880351870078740157,
+                    1258.8047687922440175935039370079,
+                    629.40238439612200879675196850394,
+                    314.70119219806100439837598425197,
+                    157.35059609903050219918799212598,
+                    78.675298049515251099593996062992,
+                    39.337649024757625549796998031496,
+                    19.668824512378812774898499015748
+                ];
+                this.resolution = [261928.06811503,130964.034057515,65482.0170287575,32741.00851437875,16370.504257189375,
+                    8185.2521285946875,4092.62606429734375,2046.313032148671875,1023.1565160743359375,511.57825803716796875,
+                    255.789129018583984375,127.8945645092919921875,63.94728225464599609375,31.973641127322998046875,15.9868205636614990234375,
+                    7.99341028183074951171875,3.996705140915374755859375,1.9983525704576873779296875,0.99917628522884368896484375,0.499588142614421844482421875];
+                this.tileInfo = new esri.layers.TileInfo({
+                    "rows": 256,
+                    "cols": 256,
+                    "compressionQuality": 90,
+                    "origin": {
+                        "x": -33526792.7187241,
+                        "y": 33526792.7187241
+                    },
+                    "spatialReference": this.spatialReference,
+                    "lods": [{ "level": 0, "resolution": this.resolution[0], "scale": this.scale[0] },
+                        { "level": 1, "resolution": this.resolution[1], "scale": this.scale[1] },
+                        { "level": 2, "resolution": this.resolution[2], "scale": this.scale[2] },
+                        { "level": 3, "resolution": this.resolution[3], "scale": this.scale[3] },
+                        { "level": 4, "resolution": this.resolution[4], "scale": this.scale[4] },
+                        { "level": 5, "resolution": this.resolution[5], "scale": this.scale[5] },
+                        { "level": 6, "resolution": this.resolution[6], "scale": this.scale[6] },
+                        { "level": 7, "resolution": this.resolution[7], "scale": this.scale[7] },
+                        { "level": 8, "resolution": this.resolution[8], "scale": this.scale[8] },
+                        { "level": 9, "resolution": this.resolution[9], "scale": this.scale[9] },
+                        { "level": 10, "resolution": this.resolution[10], "scale": this.scale[10] },
+                        { "level": 11, "resolution": this.resolution[11], "scale": this.scale[11] },
+                        { "level": 12, "resolution": this.resolution[12], "scale": this.scale[12] },
+                        { "level": 13, "resolution": this.resolution[13], "scale": this.scale[13] },
+                        { "level": 14, "resolution": this.resolution[14], "scale": this.scale[14] },
+                        { "level": 15, "resolution": this.resolution[15], "scale": this.scale[15] },
+                        { "level": 16, "resolution": this.resolution[16], "scale": this.scale[16] },
+                        { "level": 17, "resolution": this.resolution[17], "scale": this.scale[17] },
+                        { "level": 18, "resolution": this.resolution[18], "scale": this.scale[18] },
+                        { "level": 19, "resolution": this.resolution[19], "scale": this.scale[19] }
+                    ]
+                });
+                this.loaded = true;
+                this.onLoad(this);
+            },
+            getTileUrl: function (level, row, col) {
+                debugger;
+                var zoom = level - 1;
+                var offsetX = parseInt(Math.pow(2, zoom));
+                var offsetY = offsetX - 1;
+                var numX = col - offsetX, numY = (-row) + offsetY ;
+                var num = (col + row) % 8 + 1;
+                return "http://online" + num + ".map.bdimg.com/tile/?qt=tile&x="+numX+"&y="+numY+"&z="+level+"&styles=pl&scaler=1&udt=20160115";
+            }
+        });
+    });
---
+++ 3.9compact/js/dojo/BAMAP/BaseMap.js
@@ -0,0 +1,367 @@
+/**
+ * Created By Blue On 2019-07-08
+ * 地图类集合
+ * 基于 ArcGis 3.9
+ */
+define([
+    "dojo/_base/declare",
+    "esri/map",
+    "esri/geometry/Point",
+    "esri/SpatialReference",
+    "esri/dijit/Scalebar",
+    "esri/layers/WMSLayer",
+    "esri/layers/WMSLayerInfo",
+    "esri/geometry/Extent",
+    'esri/renderers/SimpleRenderer',
+    "esri/symbols/SimpleLineSymbol",
+    "esri/symbols/PictureMarkerSymbol",
+    "esri/symbols/SimpleFillSymbol",
+    "esri/Color",
+    "esri/InfoTemplate",
+    "tdt/TDTLayer",
+    "tdt/TDTAnnoLayer",
+    "tdt/ImageLayer",
+    "tdt/LandFormLayer",
+    "geojson/src/geojsonlayer",
+    "dojo/domReady!"
+], function (declare, Map, Point, SpatialReference, Scalebar, WMSLayer, WMSLayerInfo, Extent,
+    SimpleRenderer, SimpleLineSymbol, PictureMarkerSymbol, SimpleFillSymbol, Color, InfoTemplate, TDTLayer, TDTAnnoLayer, ImageLayer,
+    LandFormLayer, GeoJsonLayer) {
+        return declare('BaseMap', [Map], {
+            __version: 'V1.2.1',
+            __author: 'BLUE',
+            constructor: function (id, options) {
+                this.__MapURLarray = null;
+                this.__AttachLayers = {};
+                this.__label = true;
+            },
+            /**
+             * 初始化地图，比较特殊
+             */
+            init: function () {
+                var ctx = this;
+                new Scalebar({
+                    map: this,//地图对象  
+                    attachTo: "bottom-left",//控件的位置，右下角  
+                    scalebarStyle: "line",//ruler 比例尺样式类型  
+                    scalebarUnit: "dual"//显示地图的单位，这里是km  metric,
+                });
+                var basemap = new TDTLayer("sldt");
+                // 添加天地图矢量标注
+                var annolayer = new TDTAnnoLayer("slbz");
+                // 影像底图
+                var imagelayer = new ImageLayer("imgdt");
+                // 地形底图
+                var landformlayer = new LandFormLayer("dxdt");
+                ctx.__MapURLarray = { sldt: basemap, slbz: annolayer, imgdt: imagelayer, dxdt: landformlayer }
+                ctx.addLayer(ctx.__MapURLarray.sldt, 0);
+                ctx.addLayer(ctx.__MapURLarray.imgdt, 1);
+                ctx.addLayer(ctx.__MapURLarray.dxdt, 2);
+                ctx.addLayer(ctx.__MapURLarray.slbz, 10);
+                ctx.__MapURLarray.imgdt.hide(); ctx.__MapURLarray.dxdt.hide();
+                return this;
+            },
+            /**
+             * 切换底图
+             * @param {String} id 地图id sldt-矢量底图,imgdt-影像底图,dxdt-地形底图
+             */
+            changeBaseMap: function (id) {
+                var ctx = this;
+                for (var key in ctx.__MapURLarray) {
+                    ctx.__MapURLarray[key].hide()
+                }
+                ctx.__MapURLarray[id].show();
+                ctx.__MapURLarray.slbz.show();
+                return this;
+            },
+            /**
+             * 切换标注
+             */
+            toggleLable: function () {
+                var ctx = this;
+                if (ctx.__label) {
+                    ctx.removeLayer(ctx.__MapURLarray.slbz)
+                } else {
+                    ctx.addLayer(ctx.__MapURLarray.slbz)
+                }
+                ctx.__label = !ctx.__label;
+                return this;
+            },
+            /**
+             * 定位点，将点拉到屏幕中心
+             * @param {Number} lng 经度
+             * @param {Number} lat 纬度
+             * @param {Int} zoom 缩放层级
+             */
+            goto: function (lng, lat, zoom) {
+                zoom = zoom || this.map.getZoom();
+                var pt = new Point(lng, lat, this.spatialReference);
+                this.centerAndZoom(pt, zoom);
+                return this;
+            },
+            /**
+             * BMap 类特殊的附加图层，可用于图层管理，不建议直接使用原型的添加图层方法
+             * @param {Object} layer 附加的图层对象
+             * @param {Int} index 图层顺序
+             */
+            adbLayer: function (layer, index) {
+                // 我TMD，不能直接重写父类方法吗
+                if (layer && layer.hasOwnProperty('id')) {
+                    var ctx = this, id = layer.id
+                    if (ctx.__AttachLayers.hasOwnProperty(id)) {
+                        throw new Error("The " + id + " layer already exists");
+                    }
+                    ctx.__AttachLayers[id] = layer;
+                    if (index) {
+                        ctx.addLayer(layer, index);
+                    } else {
+                        ctx.addLayer(layer, index);
+                    }
+                    return this;
+                } else {
+                    throw new Error('This layer has no id attribute')
+                }
+            },
+
+            /**
+             * 隐藏附加图层
+             * @param {String} name 图层名称或者id
+             */
+            hideAttachLayer: function (name) {
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    this.__AttachLayers[name].hide();
+                } else {
+                    throw new Error('The layer was not found')
+                }
+                return this;
+            },
+
+            /**
+             * 显示附加图层
+             * @param {String} name 图层名称或者id
+             */
+            showAttachLayer: function (name) {
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    this.__AttachLayers[name].show();
+                } else {
+                    throw new Error('The layer was not found')
+                }
+                return this;
+            },
+
+            /**
+             * 销毁附加图层
+             * @param {String} name 图层名称或者id
+             */
+            destoryAttachLayer: function (name) {
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    this.removeLayer(this.__AttachLayers[name])
+                    delete this.__AttachLayers[name]
+                } else {
+                    throw new Error('The layer was not found')
+                }
+                return this;
+            },
+
+            /**
+             * 判断图层是否存在
+             * @param {String} name 图层名称或者id
+             * @returns {Boolean}
+             */
+            hasLayer: function (name) {
+                return Boolean(this.__AttachLayers[name])
+            },
+
+            /**
+             * 地图放大
+             * @param {Number} zoom 放大层级
+             */
+            zoomIn: function (zoom) {
+                zoom = zoom ? Math.abs(zoom) : 1;
+                cur = this.getZoom()
+                this.setZoom(cur + zoom)
+            },
+            /**
+             * 地图缩小
+             * @param {Number} zoom 缩小层级
+             */
+            zoomOut: function (zoom) {
+                zoom = zoom ? Math.abs(zoom) : 1;
+                cur = this.getZoom()
+                this.setZoom(cur - zoom)
+            },
+
+            /**
+             * 添加WMS图层
+             * @param {String} url WMS图层服务地址
+             * @param {String} name 命名空间:图层名称
+             * @param {Object} extent 坐标范围 
+             * @param {Function} callback 回调函数，参数为当前WMS图层
+             */
+            addWMSLayer(url, name, extent, callback) {
+                var ctx = this;
+                if (!url || !name) {
+                    console.error('Function addGeoJsonLayer,The URL and Name parameter is required, but it is not provided');
+                    return;
+                }
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    throw new Error("The " + name + " layer already exists");
+                }
+                var wmsLayer = new WMSLayer(url, {
+                    // format: "png",
+                    resourceInfo: {
+                        copyright: "GeoServer",
+                        extent: new Extent(extent.xmin, extent.ymin, extent.xmax, extent.ymax, ctx.spatialReference),
+                        featureInfoFormat: "text/html",
+                        layerInfos: [
+                            new WMSLayerInfo({
+                                name: name,
+                                queryable: true,
+                                showPopup: true
+                            })
+                        ],
+                        spatialReferences: [ctx.spatialReference.wkid],
+                        version: "1.1.0"
+                    },
+                    version: "1.1.0",
+                    visibleLayers: [name]
+                })
+                wmsLayer.on("error", function (response) {
+                    console.error("Error: %s", response.error.message);
+                });
+                wmsLayer.on("Load", function (layer) {
+                    // 进不来是咋回事儿
+                    callback && callback(layer)
+                });
+                setTimeout(function () { ctx.__AttachLayers[name] = wmsLayer; }, 1000)
+                ctx.addLayer(wmsLayer);
+            },
+
+            /**
+             * 根据geojson数据添加点图层
+             * @param {String} url 服务或文件的地址（同域）
+             * @param {Object} infoTemplate1 
+             * @param {Object} symbol color & width
+             * @param {Function} callback  回调参数为GraphicsLayer
+             * @param {Object} options GraphicsLayer的所有构造参数
+             * @param {Int} maxdraw 最大绘制量  default 1,000,000
+             */
+            addPointGeoJsonLayer: function (name, url, infoTemplate1, symbol, callback, options, maxdraw) {
+                var ctx = this, infoTemplate1 = infoTemplate1 || {}, symbol = symbol || {};
+                if (!url || !name) {
+                    throw new Error('Function addGeoJsonLayer,The URL and Name parameter is required, but it is not provided');
+                    return;
+                }
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    throw new Error("The " + name + " layer already exists");
+                }
+                var params = $.extend(true, { id: name, url: url, maxdraw: maxdraw || 1000000 }, options || {});
+                if (JSON.stringify(infoTemplate1) == '{}') { params['infoTemplate'] = false }
+                var geoJsonLayer = new GeoJsonLayer(params);
+                var imgurl = symbol.imgurl || '/static/map/img/local-marker.png', width = symbol.width || 10, height = symbol.height || 10;
+                var sls = new PictureMarkerSymbol(imgurl, width, height);
+                geoJsonLayer.renderer = new SimpleRenderer(sls);
+                if (JSON.stringify(infoTemplate1) !== '{}') {
+                    var infoTemplate = new InfoTemplate(infoTemplate1);
+                    geoJsonLayer.infoTemplate = infoTemplate;
+                }
+                this.addLayer(geoJsonLayer);
+                geoJsonLayer.on('load', function (layer) {
+                    ctx.__AttachLayers[name] = geoJsonLayer;
+                    callback && callback(geoJsonLayer)
+                })
+                geoJsonLayer.on('mouse-over', function (evt) {
+                    ctx.setMapCursor("pointer");
+                })
+                geoJsonLayer.on('mouse-out', function (e) {
+                    ctx.setMapCursor("default");
+                })
+                return this;
+            },
+
+            /**
+             * 根据geojson数据添加线状图层
+             * @param {String} url 服务或文件的地址（同域）
+             * @param {Object} infoTemplate1 
+             * @param {Object} symbol color & width
+             * @param {Function} callback  回调参数为GraphicsLayer
+             * @param {Object} options GraphicsLayer的所有构造参数
+             * @param {Int} maxdraw 最大绘制量  default 1,000,000
+             */
+            addLineGeoJsonLayer: function (name, url, infoTemplate1, symbol, callback, options, maxdraw) {
+                var ctx = this, infoTemplate1 = infoTemplate1 || {}, symbol = symbol || {};
+                if (!url && !name) {
+                    throw new Error('Function addGeoJsonLayer,The URL and Name parameter is required, but it is not provided');
+                }
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    throw new Error("The " + name + " layer already exists");
+                }
+                var params = $.extend(true, { id: name, url: url, maxdraw: maxdraw || 1000000 }, options || {});
+                if (JSON.stringify(infoTemplate1) == '{}') { params['infoTemplate'] = false }
+                var geoJsonLayer = new GeoJsonLayer(params);
+                var color = symbol.color || '#ff0000', width = symbol.width || 1;
+                var sls = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color(color), width);
+                geoJsonLayer.renderer = new SimpleRenderer(sls);
+                if (JSON.stringify(infoTemplate1) !== '{}') {
+                    var infoTemplate = new InfoTemplate(infoTemplate1);
+                    geoJsonLayer.infoTemplate = infoTemplate;
+                }
+                ctx.addLayer(geoJsonLayer);
+                geoJsonLayer.on('load', function (layer) {
+                    ctx.__AttachLayers[name] = geoJsonLayer;
+                    callback && callback(geoJsonLayer)
+                })
+                geoJsonLayer.on('mouse-over', function (evt) {
+                    ctx.setMapCursor("pointer");
+                })
+                geoJsonLayer.on('mouse-out', function (e) {
+                    ctx.setMapCursor("default");
+                })
+                return this;
+            },
+
+            /**
+             * 根据geojson数据添加面图层
+             * @param {String} url 服务或文件的地址（同域）
+             * @param {Object} infoTemplate1 
+             * @param {Object} symbol color & width
+             * @param {Function} callback  回调参数为GraphicsLayer
+             * @param {Object} options GraphicsLayer的所有构造参数
+             * @param {Int} maxdraw 最大绘制量  default 1,000,000
+             */
+            addGonGeoJsonLayer: function (name, url, infoTemplate1, symbol, callback, options, maxdraw) {
+                var ctx = this, infoTemplate1 = infoTemplate1 || {}, symbol = symbol || {};
+                if (!url || !name) {
+                    throw new Error('Function addGonGeoJsonLayer,The URL and Name parameter is required, but it is not provided');
+                }
+                if (this.__AttachLayers.hasOwnProperty(name)) {
+                    throw new Error("The " + name + " layer already exists");
+                }
+                var params = $.extend(true, { id: name, url: url, maxdraw: maxdraw || 1000000 }, options || {});
+                if (JSON.stringify(infoTemplate1) == '{}') { params['infoTemplate'] = false }
+                var geoJsonLayer = new GeoJsonLayer(params);
+                var color = symbol.color || '#ff0000', width = symbol.width || 1, fillcolor = symbol.fillcolor || [255, 255, 0, 0.25];
+                var sls = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+                    new SimpleLineSymbol(SimpleLineSymbol.STYLE_DASHDOT,
+                        new Color(color), width), new Color(fillcolor)
+                );
+                geoJsonLayer.renderer = new SimpleRenderer(sls);
+                if (JSON.stringify(infoTemplate1) !== '{}') {
+                    var infoTemplate = new InfoTemplate(infoTemplate1);
+                    geoJsonLayer.infoTemplate = infoTemplate;
+                }
+                this.addLayer(geoJsonLayer);
+                geoJsonLayer.on('load', function (layer) {
+                    self.__AttachLayers[name] = geoJsonLayer;
+                    callback && callback(geoJsonLayer)
+                })
+                geoJsonLayer.on('mouse-over', function (evt) {
+                    self.setMapCursor("pointer");
+                })
+                geoJsonLayer.on('mouse-out', function (e) {
+                    self.setMapCursor("default");
+                })
+            },
+
+        })
+    });
